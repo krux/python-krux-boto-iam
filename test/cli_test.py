c84c0dd824e40aced88e6590b353030b3337501d
Kelsey Lam <kelseylam@berkeley.edu>
2016-08-09 11:20:53 -0700

    Finished cli test and adding to iam_test

---
+++ test/cli_test.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+#
+# © 2016 Krux Digital, Inc.
+#
+
+#
+# Standard libraries
+#
+
+from __future__ import absolute_import
+import unittest
+import sys
+
+#
+# Third party libraries
+#
+
+from mock import MagicMock, patch
+
+#
+# Internal libraries
+#
+
+from krux_boto_iam.cli import Application, NAME, main
+from krux.stats import DummyStatsClient
+
+
+class CLItest(unittest.TestCase):
+
+    USERNAME = 'phan'
+    USER_DATA = 'test_user_data'
+
+    def setUp(self):
+        self.app = Application()
+        self.app.logger = MagicMock()
+
+    @patch('krux_boto_iam.cli.get_iam')
+    def test_init(self, mock_get_iam):
+        """
+        IAM CLI Application is initialized properly
+        """
+        # Instantiating app again in this test to check if get_iam is being called in init
+        app = Application()
+
+        # There are not much we can test except all the objects are under the correct name
+        self.assertEqual(NAME, app.name)
+        self.assertEqual(NAME, app.parser.description)
+        # The dummy stats client has no awareness of the name. Just check the class.
+        self.assertIsInstance(app.stats, DummyStatsClient)
+        self.assertEqual(NAME, app.logger.name)
+
+        mock_get_iam.assert_called_once_with(app.args, app.logger, app.stats)
+
+    @patch('krux_boto_iam.cli.pprint')
+    @patch('krux_boto_iam.cli.IAM.get_user', return_value=USER_DATA)
+    def test_run(self, mock_get_user, mock_pprint):
+        """
+        Test IAM CLI Application run
+        """
+        self.app.run()
+
+        mock_get_user.assert_called_once_with(self.USERNAME)
+        mock_pprint.assert_called_once_with(self.USER_DATA)
+
+    def test_main(self):
+        """
+        IAM CLI Application is instantiated and run() is called in main()
+        """
+        app = MagicMock()
+        app_class = MagicMock(return_value=app)
+
+        with patch('krux_boto_iam.cli.Application', app_class):
+            main()
+
+        app_class.assert_called_once_with()
+        app.run.assert_called_once_with()
--- test/iam_test.py
+++ test/iam_test.py
@@ -15,19 +15,40 @@ import unittest
 #
 
 import krux_boto.boto
-import krux_boto_iam.iam
+from krux_boto_iam.iam import IAM, get_iam, NAME
 
 
 class IAMtest(unittest.TestCase):
     TEST_REGION = 'us-west-2'
     TEST_QUEUE_NAME = 'klam-test'
 
-    def setUp(self):
-        self._iam = krux_boto_iam.iam.IAM(
+    @patch('krux_boto_iam.iam.get_stats')
+    @patch('krux_boto_iam.iam.get_logger')
+    def setUp(self, mock_logger, mock_stats):
+        self.logger = mock_logger
+        self.stats = mock_stats
+        self.iam = krux_boto_iam.iam.IAM(
             boto=krux_boto.boto.Boto3(
                 region=self.TEST_REGION
-            )
+            ),
+            logger=self.logger,
+            stats=self.stats
         )
+
+    def test_init(self):
+        self.assertIn(NAME, iam._name)
+        self.assertEqual(self.mock_logger, self.iam._logger)
+        self.assertEqual(self.mock_stats, self.iam._stats)
+
+    def test_empty_init(self):
+        """
+        Kafka Manager API Test: Checks if KafkaManagerAPI initialized property if all user
+        inputs provided.
+        """
+        iam = IAM(KafkaManagerTest._HOSTNAME, self.mock_logger, self.mock_stats)
+        self.assertIn(NAME, iam._name)
+        self.assertEqual(self.mock_logger, iam._logger)
+        self.assertEqual(self.mock_stats, iam._stats)
 
     def test_create_user(self):
         """
@@ -35,12 +56,12 @@ class IAMtest(unittest.TestCase):
         """
         # TODO: This test needs to be improved using mock and stuff. But for the interest of time,
         # let's leave it at this minimal state.
-        user = self._iam.create_user('hworld')
+        user = self.iam.create_user('hworld')
 
         self.assertIsNotNone(user)
         self.assertEqual('hworld', user['UserName'])
 
-        user = self._iam.get_user('hworld')
+        user = self.iam.get_user('hworld')
 
         self.assertIsNotNone(user)
         self.assertEqual('hworld', user['UserName'])
@@ -51,7 +72,7 @@ class IAMtest(unittest.TestCase):
         """
         # TODO: This test needs to be improved using mock and stuff. But for the interest of time,
         # let's leave it at this minimal state.
-        self._iam.delete_user('hworld')
-        user = self._iam.get_user('hworld')
+        self.iam.delete_user('hworld')
+        user = self.iam.get_user('hworld')
 
         self.assertIsNone(user)
